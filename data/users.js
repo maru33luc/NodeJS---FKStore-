ec2b6c25e5fcf56533ed90e309aee9e123129f73
maru33luc <maru33luc@gmail.com>
2023-11-24 19:43:18 -0300

    [FEAT] encriptacion de password

--- data/users.js
+++ data/users.js
@@ -1,32 +1,16 @@
 [
  {
-  "name": "Admin",
-  "lastName": "Admin",
-  "userName": "admin",
-  "email": "admin@gmail.com",
-  "password": "123456",
-  "id": 1
- },
- {
-  "name": "Marina",
-  "lastName": "Lucero",
-  "userName": "user1",
-  "email": "user1@gmail.com",
-  "password": "123456",
-  "id": 2
- },
- {
-  "name": "Juan",
-  "lastname": "Lopez",
-  "email": "juanl@gmail.com",
-  "password": "123456",
-  "id": 3
- },
- {
   "name": "Daiana",
   "lastname": "Trinidad",
   "email": "dai@gmail.com",
-  "password": "12345678",
-  "id": 4
+  "password": "$2b$10$IIVPFO0c.Ck6.91vIULH..Gm.68DiUBJ/RnTJKhE9qV/VeZA205yC",
+  "id": 1
+ },
+ {
+  "name": "Admin",
+  "lastname": "Admin",
+  "email": "admin@gmail.com",
+  "password": "$2b$10$YWfcPm9cnXvHIk5AqLgFHufFhVg/WcR5kV611m2SM/HoOo8m66adW",
+  "id": 2
  }
 ]
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,6 +1,8 @@
 // userService.js
 const fs = require('fs');
 const path = require('path');
+const bcrypt = require('bcrypt');
+const salt = bcrypt.genSaltSync(10);
 
 const usersFilePath = path.join(__dirname, '../../data/users.js');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
@@ -11,6 +13,7 @@ module.exports = {
     },
     createUser : (user) => {
         user.id = users[users.length - 1].id + 1;
+        user.password = bcrypt.hashSync(user.password, salt);
         users.push(user);
         fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
     },
@@ -21,7 +24,16 @@ module.exports = {
         return users.find(user => user.id === id);
     },
     getUserByEmailAndPass : (email, password) => {
-        return users.find(user => user.email === email && user.password === password);
+        const user = users.find(user => user.email === email);
+        if (user) {
+            if (bcrypt.compareSync(password, user.password)) {
+                return user;
+            }else{
+                return undefined;
+            }
+        }else{
+            return undefined;
+        }
     }
     
 }
