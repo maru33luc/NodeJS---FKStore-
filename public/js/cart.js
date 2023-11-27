c499b25acd058853febb82c9709baa449d31fc06
maru33luc <maru33luc@gmail.com>
2023-11-27 20:48:47 -0300

    [FIX] totales dinamicos cart view

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,5 +1,10 @@
+
 const itemCart = document.querySelectorAll('.cart-item');
 let subtotal = 0;
+let total = 0;
+const totalCart = document.getElementById('total-cart');
+const totalItemsCart = document.getElementById('cant-items-cart');
+const subtotalItems = document.getElementById('subtotal');
 
 itemCart.forEach(item => {
     const btnAdd = item.querySelector('#add');
@@ -9,30 +14,55 @@ itemCart.forEach(item => {
 
     btnAdd.addEventListener('click', () => {
         quantity.value++;
+        total++;
+        subtotal += parseFloat(itemPrice.innerHTML);
+        totalCart.innerHTML = subtotal.toFixed(2);
+        updateCart();
+        // // actualizar carrito en el servidor
+        // let cart = getCart();
+        // let item = cart.items.find(item => item.id === parseInt(itemPrice.dataset.id));
+        // if(item) {
+        //     item.quantity++;
+        // }
+        // updateCarrito(cart);
+
+
     });
 
     btnSub.addEventListener('click', () => {
-        quantity.value--;
+        if (quantity.value > 1) {
+            quantity.value--;
+            total--;
+            subtotal -= parseFloat(itemPrice.innerHTML);
+            totalCart.innerHTML = subtotal.toFixed(2);
+            updateCart();
+        }
+        // // actualizar carrito en el servidor
+        // let cart = getCart();
+        // let item = cart.items.find(item => item.id === parseInt(itemPrice.dataset.id));
+        // if(item) {
+        //     item.quantity--;
+        // }
+        // updateCarrito(cart);
+        
     });
 
+    // Calcular subtotal para cada artículo
     const subtotalItem = parseInt(quantity.value) * parseFloat(itemPrice.innerHTML);
-    console.log(subtotalItem, typeof subtotalItem);
     subtotal += subtotalItem;
-});
-
-const totalItemsCart = document.getElementById('cant-items-cart');
-const subtotalItems = document.getElementById('subtotal');
-
-
-const items = document.querySelectorAll('.item-cart-quantity');
-let total = 0;
-
-items.forEach(item => {
-    const quantity = item.querySelector('#quantity');
     total += parseInt(quantity.value);
+    totalCart.innerHTML = subtotal.toFixed(2);
 });
 
-totalItemsCart.innerHTML = total;
-subtotalItems.innerHTML = subtotal.toFixed(2);
 
 
+
+// Función para actualizar el resumen del carrito
+function updateCart() {
+    
+    totalItemsCart.innerHTML = total;
+    subtotalItems.innerHTML = subtotal.toFixed(2);
+}
+
+// Llama a la función para establecer los valores iniciales
+updateCart();
